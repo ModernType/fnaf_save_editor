3451c72aa76342a74311f61779de4db7bbea7cbf
ModernType <antonaw2004@gmail.com>
2025-06-14 22:52:06 +0300

    Added error message when compiling not for Windows

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 
 fn main() {
+    #[cfg(not(target_os = "windows"))]
+    panic!("Program should be built for windows only!");
     slint_build::compile("ui/main.slint").unwrap();
     let mut res = winresource::WindowsResource::new();
     res.set_icon("icon.ico");
